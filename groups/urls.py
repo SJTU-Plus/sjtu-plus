eb254e5fb1953ce4aff5b1f1c5682289e9221ac0
Taraxacum <john980118@163.com>
2020-08-12 15:07:06 +0800

    Provide json port

--- groups/urls.py
+++ groups/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('json/<int:root>', views.json, name='json'),
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,6 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-from .models import Category
+from .models import Category, Group
 
 
 def index(request):
@@ -9,3 +10,25 @@ def index(request):
     return render(request, 'groups/index.html', {
         'roots': list(roots)
     })
+
+
+def json(request, root: int):
+    root = Category.objects.get(id=root)
+
+    def resolve_category(category: Category):
+        return {
+            "category_id": category.id,
+            "category_name": category.name,
+            "groups": [{
+                "group_id": g.id,
+                "group_name": g.name,
+                "group_number": str(g.number)
+            } for g in Group.objects.filter(category=category)],
+            "subcategories": [
+                resolve_category(c) for c in Category.objects.filter(parent=category)
+            ]
+        }
+
+    print(resolve_category(root))
+
+    return JsonResponse(resolve_category(root))
