cdeda0bb293a756447deee47ef719a0700cd6369
Taraxacum <john980118@163.com>
2020-08-12 13:13:01 +0800

    Serve static files with django

--- GroupPlus/urls.py
+++ GroupPlus/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
-from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
 
 urlpatterns = [
     path('groups/', include('groups.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
