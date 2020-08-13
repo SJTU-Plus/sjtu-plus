8431c25565872dfb3267d6e53bdabe21d71f146d
Taraxacum <john980118@163.com>
2020-08-13 09:49:47 +0800

    admin: minor fix

--- groups/admin.py
+++ groups/admin.py
@@ -7,14 +7,14 @@ from .models import Category, Group
 class CategoryAdmin(admin.ModelAdmin):
     date_hierarchy = 'last_modified'
     readonly_fields = ('last_modified',)
-    fields = ('name', 'parent', 'last_modified')
-    list_display = ('name', 'parent', 'last_modified')
+    fields = ('id', 'name', 'parent', 'last_modified')
+    list_display = ('id', 'name', 'parent', 'last_modified')
 
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     date_hierarchy = 'last_modified'
     readonly_fields = ('last_modified',)
-    fields = ('name', 'number', 'category', 'last_modified')
+    fields = ('name', 'number', 'category', 'last_modified', 'bot_enabled', 'vacancy')
     list_display = ('name', 'number', 'category', 'last_modified', 'bot_enabled', 'vacancy')
     list_filter = ('category',)
