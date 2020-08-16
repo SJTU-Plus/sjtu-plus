ad4771f11b1efbce9f5a5e7710691a739fcbeab8
Taraxacum <john980118@163.com>
2020-08-16 21:36:52 +0800

    fix setting access

--- GroupPlus/github.py
+++ GroupPlus/github.py
@@ -11,8 +11,8 @@ logger = logging.getLogger(__name__)
 @receiver(post_save)
 def trigger_github_action(sender, instance, **kwargs):
     url = 'https://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches'
-    username = settings['GITHUB_USERNAME']
-    token = settings['GITHUB_PERSONAL_ACCESS_TOKEN']
+    username = settings.GITHUB_USERNAME
+    token = settings.GITHUB_PERSONAL_ACCESS_TOKEN
 
     resp = requests.post(
         url.format(owner='sjtu-plus', repo='sjtu-plus.github.io', workflow_id='2177133'),
