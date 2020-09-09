5854d645b8370d0d361baaf218074e45471686ba
Alex Chi <iskyzh@gmail.com>
2020-09-09 22:15:11 +0800

    migrate templates

--- groups/views.py
+++ groups/views.py
@@ -4,31 +4,9 @@ from django.shortcuts import render
 from .models import Category, Group
 
 
-def index(request):
-    roots = Category.objects.filter(parent__id=5)
-
-    return render(request, 'groups/index.html', {
-        'roots': list(roots)
-    })
-
-
-def json(request, root: int):
+def index(request, root: int):
     root = Category.objects.get(id=root)
 
-    def resolve_category(category: Category):
-        return {
-            "category_id": category.id,
-            "category_name": category.name,
-            "groups": [{
-                "group_id": g.id,
-                "group_name": g.name,
-                "group_number": str(g.number) if g.number else None,
-                "bot_enabled": g.bot_enabled,
-                "vacancy": g.vacancy,
-            } for g in Group.objects.filter(category=category)],
-            "subcategories": [
-                resolve_category(c) for c in Category.objects.filter(parent=category)
-            ]
-        }
-
-    return JsonResponse(resolve_category(root))
+    return render(request, 'groups/index.html', {
+        'roots': root
+    })
