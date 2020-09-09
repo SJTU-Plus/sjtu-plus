dbe4ef7836f3383a05db68e1cc5dfe70722a2d2b
Alex Chi <iskyzh@gmail.com>
2020-09-10 00:32:40 +0800

    fix groups url

--- SJTUPlus/settings.py
+++ SJTUPlus/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
     },
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('verify/', views.verify, name='verify')
+    path('verify/', views.verify, name='verify'),
+    path('freshmen-groups/', lambda x: redirect('/groups/5')),
+    path('subjects/', lambda x: redirect('/groups/15'))
 ]
