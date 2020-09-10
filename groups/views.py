7157ed08ef1cd4fe18f364eb68ad3a0b7f4d6e47
Alex Chi <iskyzh@gmail.com>
2020-09-10 10:50:53 +0800

    fix groups url (#10)

--- groups/views.py
+++ groups/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
-from .models import Category
+from .models import Category, Group
+
+
+def build_category(category):
+    groups = Group.objects.filter(category=category, number__isnull=False)
+    categories = Category.objects.filter(parent=category)
+    return {
+        'id': category.id,
+        'name': category.name,
+        'groups': list(groups) if groups.count() > 0 else None,
+        'subcategories': [build_category(category) for category in categories] if categories.count() > 0 else None
+    }
 
 
 def index(request, root: int):
-    root = Category.objects.get(id=root)
+    root = build_category(Category.objects.get(id=root))
 
     return render(request, 'groups/index.j2', {
         'root': root
