44fb058cbc831ac0b85f085b4e247691fd1be77d
Alex Chi <iskyzh@gmail.com>
2020-09-10 13:36:52 +0800

    fix jaccount redirect

--- verify/urls.py
+++ verify/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "verify"
 urlpatterns = [
     path('', views.index, name='index'),
     path('generate', views.generate, name='generate'),
