6edd1c6ea87eeb907b045dd7fe07fc2d3770f113
Alex Chi <iskyzh@gmail.com>
2020-09-11 20:11:32 +0800

    add alias for categories url

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('verify/', views.verify, name='verify'),
-    path('freshmen-groups/', lambda _: redirect('/categories/5')),
-    path('subjects/', lambda _: redirect('/categories/15')),
-    path('websites/', lambda _: redirect('/categories/18'))
+    path('freshmen-groups/', lambda _: redirect('/categories/freshmen-groups')),
+    path('subjects/', lambda _: redirect('/categories/subjects')),
+    path('websites/', lambda _: redirect('/categories/websites'))
 ]
