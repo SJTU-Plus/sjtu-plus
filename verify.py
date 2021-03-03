0c366a18726c7123fc43d21ef9f888bd3183c8bb
Alex Chi <iskyzh@gmail.com>
2021-03-03 11:20:54 +0800

    add utility script for verify (#40)

---
+++ verify.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+# Usage: ./verify <QQ> <token> <API key>
+
+qq = sys.argv[1]
+token = sys.argv[2]
+api_key = sys.argv[3]
+
+print(qq, token, api_key)
+
+resp = requests.post("https://plus.sjtu.edu.cn/attest/verify",
+                     json={'qq_number': qq, 'token': token}, headers={'Api-Key': api_key})
+
+print(resp.json())
